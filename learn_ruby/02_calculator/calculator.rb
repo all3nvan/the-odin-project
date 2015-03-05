2978b3388c25a7d3198c2e24155b22457ea0d740
Allen Van <all3nvan@gmail.com>
2015-03-05 18:23:40 -0500

    completed optional calculator tests

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -10,3 +10,20 @@ def sum(array)
 	return 0 if array.empty?
 	array.reduce(:+)
 end
+
+def multiply(*numbers)
+	numbers.reduce(:*)
+end
+
+def power(base, exponent)
+	product = 1
+	exponent.times do
+		product = product * base
+	end
+	product
+end
+
+def factorial(num)
+	return 1 if num == 0
+	num * factorial(num - 1)
+end
