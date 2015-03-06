249444e348407e5a4d552ffa028173d57dc7cc60
Allen Van <all3nvan@gmail.com>
2015-03-06 03:32:57 -0500

    refactor constants

--- learn_ruby/04_pig_latin/pig_latin.rb
+++ learn_ruby/04_pig_latin/pig_latin.rb
@@ -1,14 +1,16 @@
+VOWELS = %w(a e i o u)
+SINGLE_PHONEME = %w(qu)
+
 def translate(words)
 	translation = words.split.map{ |word| translate_word(word) }
 	translation.join(" ")
 end
 
 def first_vowel_index(word)
-	vowels = %w(a e i o u)
-	single_phoneme = %w(qu)
+	
 	index = 0
 	while index < word.length do
-		if vowels.include?(word[index]) && !single_phoneme.include?(word[index - 1..index])
+		if VOWELS.include?(word[index]) && !SINGLE_PHONEME.include?(word[index - 1..index])
 			break
 		end
 		index += 1
